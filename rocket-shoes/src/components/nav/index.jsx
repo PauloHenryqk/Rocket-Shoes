465967fa3ccf21db9f5de2610a7891c77c3fb1de
Paulo Henryqk <paulohgc2005@gmail.com>
2023-06-03 00:08:56 +0000

    conf do app, estrutura do projeto e inicio do componente Nav

---
+++ rocket-shoes/src/components/nav/index.jsx
@@ -0,0 +1,22 @@
+import "./style"
+
+function Nav() {
+
+    return (
+        <nav>
+            <img src="" alt=""></img>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <input type="search" placeholder="pesquisar"/>
+            <ion-icon></ion-icon>
+
+        </nav>
+    )
+
+}
+
+export default Nav
