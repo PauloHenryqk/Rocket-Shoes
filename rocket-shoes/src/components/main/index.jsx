8dfd2997a7b8b57002846342884d4f77b7e189dd
Paulo Henryqk <paulohgc2005@gmail.com>
2023-06-10 00:37:47 +0000

    09/006/2023

--- rocket-shoes/src/components/main/index.jsx
+++ rocket-shoes/src/components/main/index.jsx
@@ -3,7 +3,7 @@ import "./style.css"
 function Main() {
     return (
         <main>
-            <img src="./assets/tenis-1-ampliado.png" alt="foto tenis" />
+            <img src="./assets/tenis-1-ampliado.png" alt="foto tenis" /> 
             <div id="selection">
                 <img src="./assets/tenis-1-galeria.png" alt="" />
                 <img src="./assets/tenis-2-galeria.png" id="center-img" alt="" />
