7d0b47603155e2376ef12e007eeafe62b3da765d
Paulo Henryqk <paulohgc2005@gmail.com>
2023-06-12 23:17:26 +0000

    last commit

--- rocket-shoes/src/components/footer/index.jsx
+++ rocket-shoes/src/components/footer/index.jsx
@@ -3,8 +3,7 @@ import "./style.css"
 function Footer() {
     return (
         <footer>
-            <iframe>
-
+            <iframe width="240" height="160" src="https://www.youtube.com/embed/d1qnvK3ID_w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
             </iframe>
             <a href="#">Facebook</a>
             <a href="#">Instagram</a>
--- rocket-shoes/src/components/main/index.jsx
+++ rocket-shoes/src/components/main/index.jsx
@@ -5,9 +5,9 @@ function Main() {
         <main>
             <img src="./assets/tenis-1-ampliado.png" alt="foto tenis" /> 
             <div id="selection">
-                <img src="./assets/tenis-1-galeria.png" alt="" />
-                <img src="./assets/tenis-2-galeria.png" id="center-img" alt="" />
-                <img src="./assets/tenis-3-galeria.png" alt="" />
+                <img src="./assets/tenis-1-galeria.png" alt="imagem tenis 1" />
+                <img src="./assets/tenis-2-galeria.png" id="center-img" class="selected" alt="imagem tenis 2" />
+                <img src="./assets/tenis-3-galeria.png" alt="imagem tenis 3" />
             </div>
         </main>
     )
